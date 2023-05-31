178f2d5c34277005fd862bf51db69279f8075e39
NomanGhayyur <nomanghayyur96@gmail.com>
2023-05-31 19:53:20 +0500

    added Routes and navbar to tha app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar'
+import Create from './components/Create';
+import Update from './components/Update'
+import Read from './components/Read'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/create' element={<Create />} />
+          <Route exact path='/all' element={<Read />} />
+          <Route exact path='/update' element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
