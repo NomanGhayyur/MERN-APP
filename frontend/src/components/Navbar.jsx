5cde4b1d8d08e66211e9d5a9cafa38c6f53e992a
NomanGhayyur <nomanghayyur96@gmail.com>
2023-05-31 19:53:47 +0500

    added Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <h3 className="navbar-brand" href="#">
+          MERN
+        </h3>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link" aria-current="page">
+                Create Post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/all" className="nav-link active" aria-current="page">
+                All Post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/update"
+                className="nav-link active"
+                aria-current="page"
+              >
+                Update Post
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
