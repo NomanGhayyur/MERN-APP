419f66c9d43c125b15ee8622c732299d16057450
NomanGhayyur <nomanghayyur96@gmail.com>
2023-06-01 18:13:15 +0500

    add a home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Create from './components/Create';
 import Update from './components/Update'
 import Read from './components/Read'
+import Home from './components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route exact path='/' element={<Home />} />
           <Route exact path='/create' element={<Create />} />
           <Route exact path='/userlist' element={<Read />} />
           <Route exact path='/update/:id' element={<Update />} />
